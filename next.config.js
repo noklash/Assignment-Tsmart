5e8268897217364cd7d43cae7a0f8d1dfdf01074
noklash <123834419+noklash@users.noreply.github.com>
2023-11-12 09:42:19 +0300

    test

--- next.config.js
+++ next.config.js
@@ -3,7 +3,10 @@ const nextConfig = {
     reactStrictMode: false,
     eslint: { 
         ignoreDuringBuilds: true, 
-      }
+      },
+      typescript: {
+        ignoreBuildErrors: false,
+      },
 };
 
 module.exports = nextConfig
